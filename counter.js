207e753f582c16336df4585f1f1dba77288c9520
Duhbinchi <mapilisanrodje@gmail.com>
2024-01-14 09:24:06 +0800

    Set Interval and Local Storage

--- counter.js
+++ counter.js
@@ -1,17 +1,24 @@
-let counter = 0;
+if (!localStorage.getItem('counter')) {
+    localStorage.setItem('counter', 0);
+}
 
 function count() {
+    let counter = localStorage.getItem('counter');
+
     counter++;
-    // alert(counter);
     document.querySelector('h1').innerHTML = counter;
-    
-    if (counter % 2 != 0) {
-        alert(`${counter} is an Odd Number.`)
-    }
+
+    localStorage.setItem('counter', counter);
 }   
 
 // Wait until all content is loaded then RUN the function
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', () => {
+
+    document.querySelector('h1').innerHTML = localStorage.getItem('counter');
+
 // Without count() it means that <count> will ONLY run WHEN clicked
     document.querySelector('button').onclick = count;
+
+    // Adds for every one second
+    setInterval(count, 1000);
 });
